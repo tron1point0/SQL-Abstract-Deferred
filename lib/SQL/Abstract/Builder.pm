2611f1343dbeab314bb074988bf247c665b896c3
Traian Nedelea <tron1point0@gmail.com>
2012-04-26 15:14:38 -0500

    more of the same

--- lib/SQL/Abstract/Builder.pm
+++ lib/SQL/Abstract/Builder.pm
@@ -13,13 +13,13 @@ our @EXPORT_OK = qw(query build include);
 # ABSTRACT: Quickly build & query relational data
 # VERSION
 
-sub refp {
+sub _refp {
     return unless defined $_[0];
     return @{$_[0]} if ref $_[0] eq ref [];
     return @_;
 }
 
-sub rollup {
+sub _rollup {
     my %row = @_;
     my @fields = grep {m/\w+:\w+/} keys %row;
     for (@fields) {
@@ -29,13 +29,13 @@ sub rollup {
     %row;
 }
 
-sub smerge {
+sub _smerge {
     my ($a,$b) = @_;
     for (keys $b) {
         $a->{$_} = $b->{$_} unless defined $a->{$_};
         next if $a->{$_} eq $b->{$_};
-        $a->{$_} = [refp $a->{$_}] unless ref $a->{$_} eq ref [];
-        push @{$a->{$_}}, refp $b->{$_};
+        $a->{$_} = [_refp $a->{$_}] unless ref $a->{$_} eq ref [];
+        push @{$a->{$_}}, _refp $b->{$_};
     }
     return $a;
 }
@@ -44,7 +44,7 @@ sub query (&;@) {
     my @db = (shift)->();
     my $dbh = ref $db[0] eq 'DBIx::Simple' ? $db[0] : DBIx::Simple->connect(@db);
     my ($key,%row);
-    $row{$_->{$key}} = smerge $row{$_->{$key}}, $_ for map {{rollup %$_}}
+    $row{$_->{$key}} = _smerge $row{$_->{$key}}, $_ for map {{_rollup %$_}}
     map {my @q;($key,@q) = $_->(); $dbh->query(@q)->hashes} @_;
     values %row;
 }
@@ -53,13 +53,13 @@ sub build (&;@) {
     my ($fn,@includes) = @_;
     my %params = $fn->();
     my $table = $params{'-from'};
-    $params{'-columns'} = [map {"$table.$_"} refp $params{'-columns'}];
+    $params{'-columns'} = [map {"$table.$_"} _refp $params{'-columns'}];
     my $key = delete $params{'-key'};
     my $a = SQL::Abstract::More->new;
     map {
         my %p = %{merge \%params, {$_->()}};
         $p{'-from'} = [-join =>
-            map {ref $_ eq ref sub {} ? ($_->($table,$key)) : $_ } refp $p{'-from'}
+            map {ref $_ eq ref sub {} ? ($_->($table,$key)) : $_ } _refp $p{'-from'}
         ];
         sub {$key, $a->select(%p)};
     } @includes;
@@ -71,7 +71,7 @@ sub include (&;@) {
     my ($jtable,$jfield) = @params{qw(-from -key)};
     $params{'-columns'} = [
         map {"$jtable.$_|'$jtable:$_'"}
-        refp $params{'-columns'}
+        _refp $params{'-columns'}
     ];
     $params{'-from'} = sub {"=>{$_[0].$_[1]=$jtable.$jfield}",$jtable};
     delete $params{'-key'};
